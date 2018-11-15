6e2cc1bda3e5906ebc11d508a15bcec248df53b9
Gordon Moseley <moseleygj@gmail.com>
2018-11-15 13:40:30 -0500

    date

---
+++ dates.py
@@ -0,0 +1,11 @@
+import datetime
+
+date = datetime.datetime.now()
+print("day:"+date.strftime("%A"))
+print("month:"+date.strftime("%B"))
+print("date:"+str(date.day)+"TH")
+print("year:"+date.strftime("%Y"))
+
+
+
+
