1dbb3a1a3bb289efc03281bb3148f762b00987e9
Gordon Moseley <moseleygj@gmail.com>
2018-11-20 21:51:24 -0500

    longest word update

--- longestWord.py
+++ longestWord.py
@@ -2,11 +2,15 @@ print("Please end a sentence below:")
 userInput=input()
 i=userInput
 
-h_num=0
+h_num = 0
+
 for a in i.split(" "):
     ##print(a + " - " + str(len(a)))
-    if len(a) > h_num:
+    print(a)
+    print(h_num)
+    if len(a) >= h_num:
         h_num=len(a)
+        longest_word=a
 
 
-print("The highest number is: "+str(len(a)))
+print("The longest word is: "+longest_word +"["+str(h_num)+"]")
